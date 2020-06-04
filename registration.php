42bbe61be2e2182bdec71efeb090e6645c7f6223
RedefiningTheWeb <pradeepverma@redefiningtheweb.com>
2020-06-04 19:16:19 +0530

    updating package license

--- registration.php
+++ registration.php
@@ -1,4 +1,19 @@
 <?php
+/**
+ * Redefining The Web
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Redefiningtheweb.com license that is
+ * available through the world-wide-web at this URL: https://redefiningtheweb.com/LICENSE.txt
+ *
+ * DISCLAIMER
+ *
+ * @category    Rtw
+ * @package     Rtw_Core
+ * @copyright   Copyright (c) Rtw (https://redefiningtheweb.com/)
+ * @license     https://redefiningtheweb.com/LICENSE.txt
+ */
 
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
